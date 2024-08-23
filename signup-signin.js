6fec5dc520172b533d5fd31bdb876b8c405ae9bf
NikelRoar <timothy.patterson.nathaniel@protonmail.com>
2024-08-23 12:41:14 +0000

    commit messsage #1 added a functional hopefully login functionality

--- signup-signin.js
+++ signup-signin.js
@@ -1,1 +1,29 @@
-var signUpModal = document
+var signUpModal = document.getElementById("signUpForm");
+var signInModal = document.getElementById("loginForm");
+
+var signUpBtn = document.getElementById("signUpBtn");
+var signInBtn = document.getElementById("signInBtn");
+
+var closeButtons = document.getElementsByClassName("close");
+
+signUpBtn.onclick = function () {
+    signUpModal.style.display = "block";
+}
+signInBtn.onlick = function() {
+    signInModal.style.display = "block";
+}
+
+for (var i = 0; i < closeButtons.length; i++) {
+    closeButtons[i].onclick = function() {
+        var targetModal = document.getElementById(this.getAttribute("data-target"));
+        targetModal.style.display = "none";
+    }
+}
+
+window.onclick = function(event) {
+    if (event.target == signUpModal) {
+        signUpModal.style.display = "none";
+    } else if (event.target == signInModal) {
+        signUpModal.style.display = "none";
+    }
+}
