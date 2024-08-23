a35b395434dc9990fb2d28f4971c21277ddb6f92
NikelRoar <timothy.patterson.nathaniel@protonmail.com>
2024-08-23 03:51:30 +0000

    ended work day

--- signup-signin.js
+++ signup-signin.js
@@ -1,17 +1,1 @@
-var modal = document.getElementById("signUpForm");
-
-var btn = document.getElementById("signUpBtn");
-
-var span = document.getElementsByClassName("close")[0];
-
-btn.onclick = function() {
-    modal.style.display = "block";
-}
-span.onclick = function() {
-    modal.style.display = "none";
-}
-window.onclick = function(event) {
-    if (event.target == modal) {
-        modal.style.display = "none";
-    }
-}
+var signUpModal = document
